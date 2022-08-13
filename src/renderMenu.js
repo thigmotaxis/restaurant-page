d1b306d5a6e7b58b3514899c436613dc6b72fe89
Andrew Jennison <andrewjohnjennison@gmail.com>
2022-08-12 18:51:19 -0700

    add renderMenu module and update event listener to create tabs

---
+++ src/renderMenu.js
@@ -0,0 +1,23 @@
+import {createElement} from "./createComponents.js";
+
+
+export function renderMenu() {
+  const content = document.querySelector(".content");
+  const menuList = createElement("ul", ["menu"], content);
+  const menuItems = ["hass", "reed", "pinkerton", "espresso", "bowl"];
+  const menuItemDescriptions =
+  [`Our hass specialty! Ok, but we actually just made the ice cream from a basic avocado like you could get at any grocery store. We poured some espreso on it for you, but you probably could have made this yourself.`,
+  `Look at this round boy! He is a majestic beast, but we have tamed him and rendered him into a creamy beverage for you. Surrender to decadence!`,
+  `For the discerning weirdo. Warning: ordering this long fellow may get you added to a federal watchlist.`,
+  `For those who prefer a quick jolt of stimulant to the long-term cardiovascular health benefits provided by omega 3 fatty acids`,
+  `A bowl of avocados. Corkage fees apply`];
+  const menuPrices = [6, 8, 10, 5, 25];
+  for(let i = 0; i < menuItems.length; i++) {
+    const menuItem = createElement("li", ["menuItem"], menuList);
+    menuItem.setAttribute("id", menuItems[i]);
+    menuItem.innerHTML = `${menuPrices[i]} ${menuItems[i].slice(0, 1).toUpperCase() + menuItems[i].slice(1)}`;
+    const description = createElement("p", ["description"], menuList)
+    description.innerHTML = menuItemDescriptions[i]
+  }
+
+}
